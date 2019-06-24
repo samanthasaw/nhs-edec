a9ff47d14bfb39e9d8af74f675ab85de2a238411
Samantha Saw <samantha.saw@nhs.net>
2019-06-24 15:09:15 +0100

    Created opening hours pages

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,21 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/submit-your-edec/sections/opening-hours/check-last-year', function (req, res) {
+  let answer = req.body.openinghours;
+
+  if (answer === 'yes') {
+
+    res.redirect('/submit-your-edec/sections/opening-hours/check-special-opening-hours')
+
+  } else if (answer === 'no') {
+
+    res.redirect('/submit-your-edec/sections/opening-hours/change-opening-hours') }
+
+  else {
+    // res.redirect('error')
+    res.redirect('/submit-your-edec/errors/generic-error/')
+  }
+});
+
 module.exports = router;
